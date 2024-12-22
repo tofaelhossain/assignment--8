77242141e7bce8560c3fc0bb0d56b43e366868ec
tofael <tofaelphysics@gmail.com>
2024-12-22 22:08:42 +0600

    update

--- app/page.js
+++ app/page.js
@@ -3,16 +3,25 @@ import PopularMovie from "@/components/movies/PopularMovie";
 import TopRated from "@/components/movies/TopRated";
 import TrendingNow from "@/components/movies/TrendingNow";
 import Upcoming from "@/components/movies/UpComing";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <div className="container mx-auto px-4 py-8">
-        <TrendingNow />
-        <PopularMovie />
-        <TopRated />
-        <Upcoming />
+        <Suspense fallback={<div>Loading Trending Movies...</div>}>
+          <TrendingNow />
+        </Suspense>
+        <Suspense fallback={<div>Loading Popular Movies...</div>}>
+          <PopularMovie />
+        </Suspense>
+        <Suspense fallback={<div>Loading Top Rated Movies...</div>}>
+          <TopRated />
+        </Suspense>
+        <Suspense fallback={<div>Loading Upcoming Movies...</div>}>
+          <Upcoming />
+        </Suspense>
       </div>
     </>
   );
