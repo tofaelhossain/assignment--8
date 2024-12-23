f2a184698e3076844b6fcf9e5965a7ff74f0efcf
tofael <tofaelphysics@gmail.com>
2024-12-23 20:58:20 +0600

    header update

--- components/Header.jsx
+++ components/Header.jsx
@@ -35,6 +35,9 @@ export default function Header() {
     }
   }, [pathname]); // Listen for pathname changes
 
+  const shouldHideLinks =
+    pathname.includes("/login") || pathname.includes("/register");
+
   return (
     <nav className="fixed w-full z-50 bg-gradient-to-b from-black to-transparent">
       <div className="container mx-auto px-4 py-6 flex justify-between items-center">
@@ -46,33 +49,47 @@ export default function Header() {
             <Link href="/" className="text-white hover:text-gray-300">
               Home
             </Link>
-            <Link href="/compare" className="text-white hover:text-gray-300">
-              Compare Movies
-            </Link>
+            {!shouldHideLinks && (
+              <>
+                <Link
+                  href="/compare"
+                  className="text-white hover:text-gray-300"
+                >
+                  Compare Movies
+                </Link>
 
-            <Link href="/watchList" className="text-white hover:text-gray-300">
-              Watch Later
-            </Link>
+                <Link
+                  href="/watchList"
+                  className="text-white hover:text-gray-300"
+                >
+                  Watch Later
+                </Link>
+              </>
+            )}
           </div>
         </div>
-        <div className="flex items-center">
-          <SignInOut />
-        </div>
+        {!shouldHideLinks && (
+          <>
+            <div className="flex items-center">
+              <SignInOut />
+            </div>
 
-        <div className="relative">
-          <input
-            type="text"
-            id="searchInput"
-            placeholder="Search movies..."
-            className="bg-black bg-opacity-50 text-white px-4 py-2 rounded border border-gray-600 focus:outline-none focus:border-white"
-            value={searchQuery}
-            onChange={handleSearchChange}
-          />
-          <div
-            id="searchResults"
-            className="absolute w-full mt-2 bg-black bg-opacity-90 rounded-lg hidden"
-          ></div>
-        </div>
+            <div className="relative">
+              <input
+                type="text"
+                id="searchInput"
+                placeholder="Search movies..."
+                className="bg-black bg-opacity-50 text-white px-4 py-2 rounded border border-gray-600 focus:outline-none focus:border-white"
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+              <div
+                id="searchResults"
+                className="absolute w-full mt-2 bg-black bg-opacity-90 rounded-lg hidden"
+              ></div>
+            </div>
+          </>
+        )}
       </div>
     </nav>
   );
